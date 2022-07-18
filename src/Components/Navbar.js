1f8df68ebb49832ceafbc342e55c5a0d4dc6a583
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-07-18 15:13:01 +0100

    relative link added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -14,7 +14,14 @@ const Navbar = () => {
                     <NavLink to='/cards' className='link'>GIFT CARDS</NavLink>
                 </div>
             </div>
-            <div className="nav-card2"></div>
+            <div className="nav-card2">
+                <span>
+                    <i className="fa-solid fa-location"></i>
+                    <NavLink to='/store' className='link'>Find a store</NavLink>
+                </span>
+                <Link to='/signin'>SIgn in</Link>
+                <Link to='join'>Join now</Link>
+            </div>
         </div>
     </div>
   )
