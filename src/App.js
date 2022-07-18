cbba549e50e8a09681dbc258f7c49c8b30c0b0bc
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-07-18 15:01:56 +0100

    links and navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Rewards from './Components/Rewards';
+import Menu from './Components/Menu';
+import Cards from './Components/Cards';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+        <Routes>
+          <Route path='/reward' element={<Rewards/>}/>
+          <Route path='/menu' element={<Menu/>}/>
+          <Route path='/card' element={<Cards/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Navbar.css'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav-container'>
+        <div className="nav">
+            <div className="nav-card1">
+                <img src="./assets/bucks-logo.svg" alt="logo" />
+                <div className="links">
+                    <NavLink to='/menu'>MENU</NavLink>
+                    <NavLink to='/rewards'>REWARDS</NavLink>
+                    <NavLink to='/cards'>GIFT CARDS</NavLink>
+                </div>
+            </div>
+            <div className="nav-card2"></div>
+        </div>
+    </div>
   )
 }
 
