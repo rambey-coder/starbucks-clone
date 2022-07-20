cab8ec512de1dc9fd398eba3e06386f3e68e4176
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-07-20 14:13:50 +0100

    menu design started

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Footer from './Footer'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+      <div className="menu-container">
+      <div className="menu-nav">
+        <li>All products</li>
+        <li>Featured</li>
+        <li>Previous Order</li>
+        <li>Favorite Products</li>
+      </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
