2eca3ceb7e7b7b1b237d0b3b224170dc6d8041ac
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-07-21 11:53:01 +0100

    imzges added

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,19 +1,43 @@
 import React from 'react'
+import './Menu.css'
 import Footer from './Footer'
 
 const Menu = () => {
   return (
-    <div>
+    <>
       <div className="menu-container">
       <div className="menu-nav">
+        <ul>
         <li>All products</li>
         <li>Featured</li>
         <li>Previous Order</li>
         <li>Favorite Products</li>
+        </ul>
+      </div>
+
+      <div className="menu-content">
+        <div className="type-list">
+          <div className="list-type">
+          <h1>Drinks</h1>
+          <ul>
+            <li>Hot Coffee</li>
+            <li>Hot Teas</li>
+            <li>Hot Drinks</li>
+            <li>Frappuccino® Blended Beverages</li>
+            <li>Cold Coffees</li>
+            <li>Iced Teas</li>
+            <li>Cold Drinks</li>
+          </ul>
+          </div>
+          <div className="list-imgs">
+            <img src='./assets/hot-coffee.webp' alt='hot coffee'/>
+          </div>
+        </div>
+        <div className="menu-list"></div>
       </div>
       </div>
       <Footer/>
-    </div>
+    </>
   )
 }
 
