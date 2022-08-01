30ff8dc792e0118ede316b0fdf6913944ddf5e8e
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-08-01 11:38:07 +0100

    reward page

--- src/Components/Rewards.js
+++ src/Components/Rewards.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Reward.css'
 
 const Rewards = () => {
   return (
-    <div>Rewards</div>
+    <>
+    <div className="nav-menu">
+    <div>
+    <h2>STARBUCKS® REWARDS</h2>
+    </div>
+    </div>
+    <div className="reward-container">
+      <h1>Lorem</h1>
+    </div>
+    </>
   )
 }
 
