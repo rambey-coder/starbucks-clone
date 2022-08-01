a76b4390d3aaea348d225c1c9a540094a6b648a3
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-08-01 13:18:23 +0100

    get started done

--- src/Components/Rewards.js
+++ src/Components/Rewards.js
@@ -24,17 +24,23 @@ const Rewards = () => {
       <p>Earn Stars and get rewarded in a few easy steps.</p>
       <div className="reward-tab">
         <div className="flex">
-          <div>1</div>
+          <div>
+            <p>1</p>
+          </div>
           <h2>Create an account</h2>
           <p>To get started, <span>join now</span>. You can also <span>join in the app</span> to get access to the full range of Starbucks® Rewards benefits</p>
         </div>
         <div className="flex">
-          <div>2</div>
+          <div>
+            <p>2</p>
+          </div>
           <h2>Order and pay how you’d like</h2>
           <p>Use cash, credit/debit card or save some time and pay right through the app. You’ll collect Stars all ways. <span>Learn how</span></p>
         </div>
         <div className="flex">
-          <div>3</div>
+          <div>
+            <p>3</p>
+          </div>
           <h2>Earn Stars, get Rewards</h2>
           <p>As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars!</p>
         </div>
